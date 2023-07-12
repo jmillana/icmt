ea1ad684e07b628c29befb8784bc6f9c06e120a9
jmillana <jordi@marimon.tech>
2023-07-12 20:54:08 +0200

    ✨ feat: Add Config struct and implementation
    - Added a new file `config.rs` with the `Config` struct and its implementation.
    - The `Config` struct contains fields for `api_key`, `api_base`, `shell`, and `max_tokens`.
    - Implemented the `new` function for the `Config` struct to initialize the fields using environment variables.
    - Implemented the `write_to_history` function for the `Config` struct to write code to the shell history file.
    
    ✨ feat: Add main.rs file
    
    - Added a new file `main.rs` with the main function.
    - Imported necessary modules and libraries.
    - Defined the `Cli` struct using the `clap` crate for command line parsing.
    - Implemented the `Display` trait for the `Mode` enum.
    - Implemented the `main` function to handle different modes based on user input.
    - Added helper functions for pretty printing, asking for confirmation, and running commands.
    - Implemented the `commit_workflow` function to handle the commit mode.
    - Added logic to generate a commit message, replace gitmoji if enabled, and execute the commit command.

---
+++ src/config.rs
@@ -0,0 +1,48 @@
+use colored::Colorize;
+use std::{env, io::Write, process::exit};
+
+const MAX_TOKENS: usize = 1000;
+
+pub struct Config {
+    pub api_key: String,
+    pub api_base: String,
+    pub shell: String,
+    pub max_tokens: usize,
+}
+
+impl Config {
+    pub fn new() -> Self {
+        let api_key = env::var("OPENAI_API_KEY").unwrap_or_else(|_| {
+            println!("{}", "This program requires an OpenAI API key to run. Please set the OPENAI_API_KEY environment variable. https://github.com/m1guelpf/plz-cli#usage".red());
+            exit(1);
+        });
+        let api_base = env::var("OPENAI_API_BASE")
+            .unwrap_or_else(|_| String::from("https://api.openai.com/v1"));
+        let shell = env::var("SHELL").unwrap_or_else(|_| String::new());
+
+        Self {
+            api_key,
+            api_base,
+            shell,
+            max_tokens: MAX_TOKENS,
+        }
+    }
+
+    pub fn write_to_history(&self, code: &str) {
+        let history_file = match self.shell.as_str() {
+            "/bin/bash" => std::env::var("HOME").unwrap() + "/.bash_history",
+            "/bin/zsh" => std::env::var("HOME").unwrap() + "/.zsh_history",
+            _ => return,
+        };
+
+        std::fs::OpenOptions::new()
+            .append(true)
+            .open(history_file)
+            .map_or((), |mut file| {
+                file.write_all(format!("{code}\n").as_bytes())
+                    .unwrap_or_else(|_| {
+                        exit(1);
+                    });
+            });
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,158 @@
+#![warn(clippy::all, clippy::pedantic, clippy::nursery)]
+
+use bat::PrettyPrinter;
+use clap::Parser;
+use colored::Colorize;
+use config::Config;
+use question::{Answer, Question};
+use spinners::{Spinner, Spinners};
+use std::process::Command;
+
+mod chat_gpt;
+mod config;
+mod git;
+mod prompts;
+
+use crate::chat_gpt::completions;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Cli {
+    /// Description of the command to execute
+    prompt: Vec<String>,
+
+    #[clap(short, long, default_value = "commit")]
+    mode: String,
+
+    // Gitmoji support
+    #[clap(short = 'e', long)]
+    gitmoji: bool,
+
+    #[clap(short, long)]
+    token_limit: Option<usize>,
+
+    #[clap(short = 'H', long)]
+    hint: Option<String>,
+}
+
+pub enum Mode {
+    Commit,
+}
+
+impl std::fmt::Display for Mode {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Mode::Commit => write!(f, "commit"),
+        }
+    }
+}
+
+fn main() {
+    println!(
+        "{} {}",
+        "🤖".bright_green(),
+        "Welcome to commit AI!".bright_green()
+    );
+    let cli = Cli::parse();
+    let config = Config::new();
+    let chat_completions = completions::ChatCompletions::new(cli, config);
+
+    match chat_completions.mode {
+        Mode::Commit => {
+            commit_workflow(chat_completions);
+        }
+    }
+}
+
+fn pprint(data: &String, lang: &str) {
+    PrettyPrinter::new()
+        .input_from_bytes(data.as_bytes())
+        .language(lang)
+        .grid(true)
+        .print()
+        .unwrap();
+}
+
+fn ask_for_confirmation(display: &str, default_answer: Option<Answer>) -> bool {
+    let defaul_answer = default_answer.unwrap_or(Answer::YES);
+    return Question::new(display)
+        .yes_no()
+        .until_acceptable()
+        .default(defaul_answer)
+        .ask()
+        .expect("Couldn't ask question.")
+        == Answer::YES;
+}
+
+fn commit_workflow(mut chat_completions: completions::ChatCompletions) {
+    let mut spinner = Spinner::new(
+        Spinners::BouncingBar,
+        "Generating your commit message...".into(),
+    );
+    chat_completions.set_system_prompt(prompts::SystemPrompt::Commit);
+
+    let commit_changes = git::get_commit_changes().unwrap_or_else(|| {
+        spinner.stop_and_persist(
+            "✖".red().to_string().as_str(),
+            "Failed to get commit changes.".red().to_string(),
+        );
+        std::process::exit(1);
+    });
+
+    let prompt = prompts::get_commit_user_prompt(commit_changes, &chat_completions.cli.hint);
+    let mut commit_message = chat_completions.refine_loop(prompt, &mut spinner);
+
+    if chat_completions.cli.gitmoji {
+        commit_message = git::replace_gitmoji(commit_message);
+    }
+
+    pprint(&commit_message, "bash");
+
+    let accept_commit = ask_for_confirmation(">> Accept the generated commit? [Y/n]", None);
+
+    if accept_commit {
+        let mut commit_cmd = "git commit -m '".to_string();
+        commit_cmd.push_str(commit_message.as_str());
+        commit_cmd.push_str("'");
+
+        pprint(&commit_cmd, "bash");
+
+        let should_run = ask_for_confirmation(">> Run the generated commit? [Y/n]", None);
+
+        if should_run {
+            spinner = Spinner::new(Spinners::BouncingBar, "Executing...".into());
+            let (stdout, _) = run_cmd(&commit_cmd, &"bash", &mut spinner);
+
+            spinner.stop_and_persist(
+                "✔".green().to_string().as_str(),
+                "Command ran successfully".green().to_string(),
+            );
+
+            println!("{}", String::from_utf8_lossy(&stdout));
+        }
+    }
+}
+
+fn run_cmd(command: &str, shell: &str, spinner: &mut Spinner) -> (Vec<u8>, Vec<u8>) {
+    let output = Command::new(shell)
+        .arg("-c")
+        .arg(command)
+        .output()
+        .unwrap_or_else(|_| {
+            spinner.stop_and_persist(
+                "✖".red().to_string().as_str(),
+                "Failed to execute the generated program.".red().to_string(),
+            );
+            std::process::exit(1);
+        });
+
+    if !output.status.success() {
+        spinner.stop_and_persist(
+            "✖".red().to_string().as_str(),
+            "The program threw an error.".red().to_string(),
+        );
+        std::process::exit(1);
+    }
+
+    return (output.stdout, output.stderr);
+}
