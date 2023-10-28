dbe272ecd816b381497a66a9980a1403ff79f42b
jmillana <jordi@marimon.tech>
2023-10-28 19:28:50 +0200

    ✨ (squash): Add squash workflow function
    - Added `squash_workflow` function to handle the process of squashing commit messages
    - Retrieves the current branch name and the base branch
    - Retrieves the commits ahead of the base branch
    - Generates a squash message based on the commit logs
    - Replaces gitmoji in the commit message if `args.gitmoji` is true
    - Resets the head of the branch and commits if auto-accept or user confirms
    - Squashes the commits if auto-accept or user confirms

---
+++ src/commands/squash.rs
@@ -0,0 +1,87 @@
+use crate::ai::completions::chatgpty;
+use crate::cli::utils::ask_for_confirmation;
+use crate::cli::SquashArgs;
+use crate::commands;
+use crate::git;
+use crate::pprint;
+use crate::prompts;
+use colored::Colorize;
+use spinners::{Spinner, Spinners};
+
+pub fn squash_workflow(mut chat_completions: chatgpty::GptyCompletions, args: &SquashArgs) {
+    // Get the current brach
+    let mut spinner = Spinner::new(Spinners::BouncingBar, "Squashing commit messages...".into());
+    let auto_accept = args.auto_accept;
+    let branch_name = git::get_branch_name().unwrap_or_else(|| {
+        spinner.stop_and_persist(
+            "✖".red().to_string().as_str(),
+            "Failed to get branch name.".red().to_string(),
+        );
+        std::process::exit(1);
+    });
+    let base_branch = &args.base_branch;
+
+    log::info!("Branch name: {:?}", branch_name);
+    if branch_name == *base_branch {
+        println!(
+            "{} {}",
+            "⚠️".bright_yellow(),
+            "Squashing against the same branch. Unable to load commits.".bright_yellow()
+        );
+    }
+    // Get commits ahead of the given branch
+    let commit_logs = git::get_commits(base_branch).unwrap_or_else(|| {
+        spinner.stop_and_persist(
+            "✖".red().to_string().as_str(),
+            "Failed to get commits.".red().to_string(),
+        );
+        std::process::exit(1);
+    });
+    chat_completions.system_prompt = prompts::squash_system_prompt(true);
+    log::info!("Commits: {:?}", commit_logs);
+    let n_commits = commit_logs.len();
+    // Generate the squash message
+    let prompt = prompts::get_squash_user_prompt(commit_logs, &args.hint);
+    let should_refine = !auto_accept;
+    let mut commit_message = chat_completions.refine_loop(prompt, should_refine, &mut spinner);
+    println!("Commit message: {:?}", commit_message);
+    //
+    if args.gitmoji {
+        commit_message = git::replace_gitmoji(commit_message);
+    }
+    //
+    spinner.stop_and_persist(
+        "✔".green().to_string().as_str(),
+        "Successfully loaded commits.".green().to_string(),
+    );
+    if auto_accept
+        || ask_for_confirmation(
+            format!(">> Reset branch head to {}? [Y/n]", base_branch).as_str(),
+            None,
+        )
+    {
+        // Reset the head of the branch and commit
+        pprint(&format!("git reset HEAD~{}", n_commits), "bash");
+        if args.dryrun {
+            println!("Dryrun: branch head have not been modified");
+        } else {
+        }
+    }
+    if auto_accept || ask_for_confirmation(">> Squash the commits? [Y/n]", None) {
+        let mut commit_cmd = "git commit -m '".to_string();
+        commit_cmd.push_str(commit_message.as_str());
+        commit_cmd.push_str("'");
+
+        pprint(&commit_cmd, "bash");
+        if args.dryrun {
+            println!("Changes have not been applied");
+            return;
+        }
+        let args = commands::RunArgs::build(commit_cmd);
+
+        match commands::run(args) {
+            Ok(output) => println!("{}", output.stdout),
+            Err(err) => println!("{}", err),
+        }
+    }
+}
