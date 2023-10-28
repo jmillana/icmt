44647c496352b77669e50c428dc938770b6a277b
jmillana <jordi@marimon.tech>
2023-10-28 22:27:23 +0200

    ♻️ refactor(squash): Refactor squash_workflow function
    - Refactored the code to improve readability and maintainability
    - Renamed variables for clarity
    - Removed unnecessary log statements
    - Replaced deprecated function calls
    - Updated formatting of git reset command

--- src/commands/squash.rs
+++ src/commands/squash.rs
@@ -30,30 +30,27 @@ pub fn squash_workflow(mut chat_completions: chatgpty::GptyCompletions, args: &S
         );
     }
     // Get commits ahead of the given branch
-    let commit_logs = git::get_commits(base_branch).unwrap_or_else(|| {
+    let commit_history = git::get_commits(base_branch);
+    if commit_history.commits.len() == 0 {
         spinner.stop_and_persist(
             "✖".red().to_string().as_str(),
             "Failed to get commits.".red().to_string(),
         );
         std::process::exit(1);
-    });
+    }
     chat_completions.system_prompt = prompts::squash_system_prompt(true);
-    log::info!("Commits: {:?}", commit_logs);
-    let n_commits = commit_logs.len();
     // Generate the squash message
-    let prompt = prompts::get_squash_user_prompt(commit_logs, &args.hint);
+    let mut commits = Vec::new();
+    let n_commits = commit_history.commits.len();
+    for commit in commit_history.commits {
+        commits.push(commit.to_string());
+    }
+    let prompt = prompts::get_squash_user_prompt(commits, &args.hint);
     let should_refine = !auto_accept;
     let mut commit_message = chat_completions.refine_loop(prompt, should_refine, &mut spinner);
-    println!("Commit message: {:?}", commit_message);
-    //
     if args.gitmoji {
         commit_message = git::replace_gitmoji(commit_message);
     }
-    //
-    spinner.stop_and_persist(
-        "✔".green().to_string().as_str(),
-        "Successfully loaded commits.".green().to_string(),
-    );
     if auto_accept
         || ask_for_confirmation(
             format!(">> Reset branch head to {}? [Y/n]", base_branch).as_str(),
@@ -61,7 +58,7 @@ pub fn squash_workflow(mut chat_completions: chatgpty::GptyCompletions, args: &S
         )
     {
         // Reset the head of the branch and commit
-        pprint(&format!("git reset HEAD~{}", n_commits), "bash");
+        pprint(&format!("git reset HEAD~{} ", n_commits), "bash");
         if args.dryrun {
             println!("Dryrun: branch head have not been modified");
         } else {
